c03967f081cdf18910befa474a565d15ce0c0240
motsat <konpeiex@gmail.com>
2011-04-27 09:16:39 +0900

    chat prototyp

--- server.js
+++ server.js
@@ -1,29 +1,56 @@
-var http          = require('http'),
+var //http          = require('http'),
     io            = require('socket.io'),
-    connect_count = 0
-    messages      = [];
+    express       = require('express'),
+    messages      = [],
+    messageType   = {'GET_DATA' : 0, 'STRING' : 1} ;    // GET=取得要求、 STRING=チャットメッセージ
 
-server = http.createServer(function(req, res){
-    console.log('[http] on connection ' + connect_count);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello world</h1>'); 
-    });
+var app = express.createServer();
+app.set("view options", { layout: false });
+app.set("views", __dirname + '/public');
+app.get('/', function(req, res) {
+    res.render('index.ejs');
+});
 
-server.listen(8000);
+app.listen(8000);
 
-var socket = io.listen(server); 
 
+//var socket = io.listen(server);
+var socket = io.listen(app);
 socket.on('connection', function(client){
     client.on('message',    function(msg) {onMessage(msg, client);});
-    client.on('disconnect', function() {console.log('on disconnect ');});
+    client.on('disconnect', function()    {onDisconnect(client)});
+});
+function onDisconnect(client) {
+  msg  = {"data"      : client. sessionId+"が切断されました",
+          "type"      : messageType.STGING,
+          "option"    : {"color" : "red"},
+          "createdAt" : getNowDate()} ;
+  socket.broadcast(msg);
 }
-);
+function addMessage(msg)
+{
+    msg.id        = messages.length + 1;
+    msg.createdAt = getNowDate();
+    if (msg.option.color == '') {
+      msg.option.color = 'black';
+    }
+    messages.push(msg);
 
-function onMessage(msg, client) {
-  // console.log(JSON.parse(msg));
-  // console.log('onMessage - ' + msg);
-  msg.sessionId = client.sessionId;
-  //var tmp = {"sessionId" : client.sessionId, "data" : msg};
-  messages.push(msg);
-  client.broadcast(JSON.stringify(msg));
+    return msg;
+}
+function onMessage(msg, client)
+{
+  if (msg.type == messageType.GET_DATA) {
+    if (typeof msg.more_id == 'number') {
+        client.send(messages.slice(msg.more_id));
+    }
+  } else {
+    addMessage(msg);
+    client.send(msg);
+    client.broadcast(msg);
+  }
+}
+function getNowDate(){
+  d = new Date();
+  return  d.getFullYear() + "-" + (d.getMonth()+1) + "-" + d.getDate()+ " "+ d.getHours()+':' +d.getMinutes()+':' + d.getSeconds();
 }
