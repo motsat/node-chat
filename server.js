8d9c9033dc204437e7c7e5fbcc7b5e5e93fdd315
motsat <konpeiex@gmail.com>
2011-04-28 09:49:28 +0900

    comment delete

--- server.js
+++ server.js
@@ -7,22 +7,14 @@ var io            = require('socket.io'),
 var app = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
-  //app.set('view engine', 'jade');
   app.use(express.static(__dirname + '/public'));
   app.set("view options", { layout: false });
-  //app.set("views", __dirname + '/views');
   app.get('/', function(req, res) {
     res.render('index.ejs');
     });
 });
 
-
-
-
-
-
 app.listen(8000);
-
 
 var socket = io.listen(app);
 socket.on('connection', function(client){
